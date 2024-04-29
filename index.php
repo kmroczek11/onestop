b61c5db85b84a0fb8f81ab5e31ec5189a5eb183a
kmroczek11 <kamil.mroczek@onet.eu>
2024-04-29 16:21:34 +0200

    Implemented routing

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'Hi there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::run($path);
